8e4216f405e7df6739e2943543711781d0f5dc97
王哲 <413118324@qq.com>
2023-04-13 21:45:20 +0800

    定义日志组件

--- applications/extensions/__init__.py
+++ applications/extensions/__init__.py
@@ -7,6 +7,7 @@ from .init_mail import init_mail, mail as flask_mail
 from .init_apscheduler import init_scheduler
 from .init_upload import init_upload
 from .init_dotenv import init_dotenv
+from .init_logger import init_logger
 
 
 def init_plugs(app: Flask) -> None:
@@ -14,6 +15,7 @@ def init_plugs(app: Flask) -> None:
     init_template_directives(app)
     init_error_views(app)
     init_mail(app)
+    init_logger(app)
     init_scheduler(app)
     init_upload(app)
     init_dotenv()
